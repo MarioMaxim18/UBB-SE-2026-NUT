d3bb06056e6e424b5238deac783aea0aa9be49be
trueprodprog <trueproductm@gmail.com>
2026-04-02 19:01:36 +0300

    updated string connection

--- TeamNut/Repositories/DbConfig.cs
+++ TeamNut/Repositories/DbConfig.cs
@@ -8,6 +8,6 @@ namespace TeamNut.Repositories
 {
     internal class DbConfig
     {
-        public static string ConnectionString => @"Data Source=np:\\.\pipe\LOCALDB#9B005CFF\tsql\query;Initial Catalog=NUTdb;Integrated Security=True;Encrypt=False;TrustServerCertificate=True;";
+        public static string ConnectionString => @"Server=(localdb)\TeamNutInstance;Database=NUTdb;Trusted_Connection=True;TrustServerCertificate=True;";
     }
 }
